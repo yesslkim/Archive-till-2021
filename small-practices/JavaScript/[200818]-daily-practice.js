0d0776a595a39d905d6fbc5657acb823d8fc045d
yesslkim <69078989+yesslkim@users.noreply.github.com>
2020-08-18 03:44:58 +0900

    Create [200818]-daily-practice.js

---
+++ small-practices/JavaScript/[200818]-daily-practice.js
@@ -0,0 +1,24 @@
+// W3School 에서 제공하는 daily exercise로 오늘 공부한거 복습하기
+
+// Execute the function named myFunction.
+
+function myFunction() {
+  alert("Hello World");
+}
+
+myFunction; // this is my answer
+
+// Create a function called "myFunction".
+
+function myFunction() {
+  // this function is my answer
+  alert("Hello World");
+}
+
+// Make the function return "Hello".
+
+function myFunction() {
+  return "Hello"; //this is my answer
+}
+
+document.getElementById("demo").innerHTML = myFunction();
