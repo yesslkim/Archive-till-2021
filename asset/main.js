f06e1968402b68c7f1270e51ad9215e9ec2656e3
yesslkim <yskim940914@gmail.com>
2020-09-17 21:14:09 +0900

    Add input that adds the TODO List

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,5 @@
+// 삭제 및 할일완료 버튼
+
 const todo = document.querySelector(".todo");
 
 todo.addEventListener("click", (e) => {
@@ -11,4 +13,29 @@ todo.addEventListener("click", (e) => {
     todoText.classList.add("line-through");
     todoList.classList.add("complete-ico");
   }
+});
+
+// TODO 추가
+
+const addForm = document.querySelector(".add-form");
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let todoText = addForm.add.value;
+  const todoTemplate = `
+<li class="todo-list">
+<p class="list-text">${todoText}</p>
+<div class="todo-btn">
+  <button type="button" class="check-btn btn">
+    <span class="sr-only">list is done</span>
+  </button>
+  <button type="button" class="delete-btn btn">
+    <span class="sr-only">delete the list</span>
+  </button>
+</div>
+
+</li>
+`;
+  todo.innerHTML += todoTemplate;
+  addForm.reset();
 });
