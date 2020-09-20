614938e5a8cdaaaf39c1f1c06cf3491dea59c433
yesslkim <yskim940914@gmail.com>
2020-09-21 00:52:03 +0900

    Update JS for a click event to the buttons

--- [200920]-Login-form/assets/main.js
+++ [200920]-Login-form/assets/main.js
@@ -5,3 +5,10 @@ const loginForm = document.querySelector(".login-form");
 loginButton.addEventListener("click", () => {
   loginDisplay.classList.remove("hidden");
 });
+
+loginForm.addEventListener("click", (e) => {
+  const targetClass = e.target.className;
+  if (targetClass === "close-btn" || targetClass === "cancel-btn") {
+    loginDisplay.classList.add("hidden");
+  }
+});
