e971a7a608104295929a862c1b4412c21d34763a
yesslkim <yskim940914@gmail.com>
2020-10-01 03:53:36 +0900

    Upadate greetingJS - create a function to get a name
    - this function is to get a name when there is no information on user name in a local storage

--- assets/JS/greetings.js
+++ assets/JS/greetings.js
@@ -3,6 +3,16 @@ const userName = document.querySelector(".js-userinfo input");
 const greetings = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 
+const getName = () => {
+  userInfoForm.classList.add("visible");
+  userInfoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const currentUser = userName.value;
+    saveName(currentUser);
+    greetUser(currentUser);
+  });
+};
+
 const loadName = () => {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
