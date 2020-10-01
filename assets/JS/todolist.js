522584f25d89ee427e6c351e34cbcb46aaa746ce
yesslkim <yskim940914@gmail.com>
2020-10-02 03:27:39 +0900

    Update todolistJS - function to get todo list from the user

--- assets/JS/todolist.js
+++ assets/JS/todolist.js
@@ -3,6 +3,15 @@ const todoInput = document.querySelector(".js-todoform input");
 const todoLists = document.querySelector(".js-todolist");
 const TODO_LS = "todolist";
 
+const getTodo = () => {
+  todoForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const todoItem = todoInput.value;
+    paintTodo(todoItem);
+    todoInput.value = "";
+  });
+};
+
 const loadTodo = () => {
   const currentTodos = localStorage.getItem(TODO_LS);
 
