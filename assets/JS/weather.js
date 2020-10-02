bf517a65ce2957044b35c489ea110006e74e26fd
yesslkim <yskim940914@gmail.com>
2020-10-02 14:37:55 +0900

    Update weatherJS- set the function to get geolocation
    - with callback functions for success and error

--- assets/JS/weather.js
+++ assets/JS/weather.js
@@ -1,5 +1,25 @@
 const COORDS = "coords";
 
+const handleGeoSuccess = position => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const CoordsObj = {
+    latitude,
+    longitude,
+  };
+  saveGeolocation(CoordsObj);
+};
+
+const handleGeoError = error => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.log(`ERROR CODE : ${errorCode}, ERROR MESSAGE : ${errorMessage}`);
+};
+
+const getGeolocation = () => {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+};
+
 const loadGeolocation = () => {
   const currentCoords = localStorage.getItem(COORDS);
 
