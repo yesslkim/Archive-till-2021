6ddd257ba40a76548b77fa68026c3c77652402f9
yesslkim <yskim940914@gmail.com>
2020-10-13 00:01:00 +0900

    Add Harry Potter JS - HTML template for display in HTML file

--- [201012]-Harry-Potter-API/main.js
+++ [201012]-Harry-Potter-API/main.js
@@ -6,6 +6,24 @@ const getItem = async() => {
   return data;
 }
 
+const createHTMLTemplate = (item) => {
+  return `
+  <li class="character-list">
+  <img src=${item.image} alt=${item.name} class="character-img">
+  <p class="character-desc">
+    Name : ${item.name} <br/>
+    Gender : ${item.gender} <br/>
+    House : ${item.house}
+  </p>
+</li>
+  `
+}
+
+const displayCharacters = (items) => {
+  const characterContainer = document.querySelector('.characters')
+  characterContainer.innerHTML = items.map(item => createHTMLTemplate(item)).join('');
+}
+
 getItem()
 .then(items => displayCharacters(items))
 .catch(err=>console.log(err))
