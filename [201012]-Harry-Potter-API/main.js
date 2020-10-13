31401d17fde58639a3778fdf4e7802393fb4eca1
yesslkim <yskim940914@gmail.com>
2020-10-14 00:06:59 +0900

    Fix Harry Potter JS - mixed content issue

--- [201012]-Harry-Potter-API/main.js
+++ [201012]-Harry-Potter-API/main.js
@@ -1,6 +1,6 @@
 //Get characters using Fetch API
 const getCharacters = async() => {
-  const response = await fetch('http://hp-api.herokuapp.com/api/characters');
+  const response = await fetch('https://hp-api.herokuapp.com/api/characters');
   const characters = await response.json()
   return characters;
 }
