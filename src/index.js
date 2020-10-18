b76966c55a2f232a98105e32637f5151ca007229
yesslkim <yskim940914@gmail.com>
2020-10-18 23:59:20 +0900

    Add dropdown by import and create dropdown

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import Tooltip from './UI-Components/tooltip';
+import Dropdown from './UI-Components/dropdown';
 
 //create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+//create a dropdown 
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown=>{
+  const instance = new Dropdown(dropdown);
+  instance.init();
+})
