7b808d35c94252310959fc8b110fe2ef10e084c7
yesslkim <yskim940914@gmail.com>
2020-10-19 00:32:04 +0900

    Add tabs to JS make active

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './UI-Components/tooltip';
 import Dropdown from './UI-Components/dropdown';
+import Tabs from './UI-Components/tabs';
 
 //create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -11,3 +12,7 @@ dropdowns.forEach(dropdown=>{
   const instance = new Dropdown(dropdown);
   instance.init();
 })
+
+//create a tab
+const tabs = new Tabs(document.querySelector('.tab-wrap'));
+tabs.init();
