338757242a098041d72e6f8e876fd09442c55564
yesslkim <yskim940914@gmail.com>
2020-11-02 23:06:35 +0900

    Create 리액트 실습용 폴더, CRA 개발환경 세팅

---
+++ React/00_practice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-nav">
+        <div>예슬의 블로그</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
