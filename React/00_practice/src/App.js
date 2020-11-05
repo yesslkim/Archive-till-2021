4093f9b5690a6dd29692e0caf74affccba18be4c
yesslkim <yskim940914@gmail.com>
2020-11-05 23:55:57 +0900

    Add useState()를 통해 블로그 제목 만들어보는 연습

--- React/00_practice/src/App.js
+++ React/00_practice/src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // let posts = '블로그 제목'; 
+  // useState()사용해보기.
+  let [blogTitle, blogEdit] = useState(['가을 노래 추천','인절미 호떡 맛나게 굽는 법', '공부를 열심히 해야하는 3가지 이유']);
   return (
     <div className="App">
       <div className="main-nav">
         <div>예슬의 블로그</div>
+      </div>
+      <div className="blog-list">
+         <h2> { blogTitle[0] } </h2>
+         <span>2020년 11월 5일</span>
+      </div>
+      <div className="blog-list">
+         <h2> { blogTitle[1] } </h2>
+         <span>2020년 11월 5일</span>
+      </div>
+      <div className="blog-list">
+         <h2> { blogTitle[2] } </h2>
+         <span>2020년 11월 5일</span>
       </div>
     </div>
   );
