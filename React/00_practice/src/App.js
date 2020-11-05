4920106dfb4b15c6f6840d0a21c11392bf080d72
yesslkim <yskim940914@gmail.com>
2020-11-06 00:29:09 +0900

    Add useState()를 통해 like버튼 기능 만들어보기

--- React/00_practice/src/App.js
+++ React/00_practice/src/App.js
@@ -6,13 +6,17 @@ function App() {
   // let posts = '블로그 제목'; 
   // useState()사용해보기.
   let [blogTitle, blogEdit] = useState(['가을 노래 추천','인절미 호떡 맛나게 굽는 법', '공부를 열심히 해야하는 3가지 이유']);
+  let [like, likeCount] = useState(0)
   return (
     <div className="App">
       <div className="main-nav">
         <div>예슬의 블로그</div>
       </div>
       <div className="blog-list">
-         <h2> { blogTitle[0] } </h2>
+        <div>
+          <h2> { blogTitle[0] }</h2>
+  <span onClick = {()=>{likeCount(like+1)}}>좋아요 {like} </span>
+        </div>
          <span>2020년 11월 5일</span>
       </div>
       <div className="blog-list">
