87d0da628d9e90b713eed92769ded00651fb7459
yesslkim <yskim940914@gmail.com>
2020-11-06 10:55:44 +0900

    Add useState()를 통해 변경하는 법 공부

--- React/00_practice/src/App.js
+++ React/00_practice/src/App.js
@@ -5,26 +5,34 @@ import './App.css';
 function App() {
   // let posts = '블로그 제목'; 
   // useState()사용해보기.
-  let [blogTitle, blogEdit] = useState(['가을 노래 추천','인절미 호떡 맛나게 굽는 법', '공부를 열심히 해야하는 3가지 이유']);
-  let [like, likeCount] = useState(0)
+  let [blogTitles, blogEdit] = useState(['가을 노래 추천','인절미 호떡 맛나게 굽는 법', '공부를 열심히 해야하는 3가지 이유']);
+  let [like, likeCount] = useState(0);
+
+  function sortBlogTitles(){
+    const cloneblogTitles = [...blogTitles];
+    cloneblogTitles.sort();
+    blogEdit(cloneblogTitles);
+  }
+
   return (
     <div className="App">
       <div className="main-nav">
         <div>예슬의 블로그</div>
       </div>
+      <button type="button" onClick= { sortBlogTitles }>블로그 정렬</button>
       <div className="blog-list">
         <div>
-          <h2> { blogTitle[0] }</h2>
-  <span onClick = {()=>{likeCount(like+1)}}>좋아요 {like} </span>
+          <h2> { blogTitles[0] }</h2>
+          <span onClick = {()=>{likeCount(like+1)}}>좋아요 {like} </span>
         </div>
          <span>2020년 11월 5일</span>
       </div>
       <div className="blog-list">
-         <h2> { blogTitle[1] } </h2>
+         <h2> { blogTitles[1] } </h2>
          <span>2020년 11월 5일</span>
       </div>
       <div className="blog-list">
-         <h2> { blogTitle[2] } </h2>
+         <h2> { blogTitles[2] } </h2>
          <span>2020년 11월 5일</span>
       </div>
     </div>
