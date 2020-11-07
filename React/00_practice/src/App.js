9527e3b63dd1d5e75a1b70a4bcbe81802510e325
yesslkim <yskim940914@gmail.com>
2020-11-07 15:30:53 +0900

    Add 모달창 띄우기 연습

--- React/00_practice/src/App.js
+++ React/00_practice/src/App.js
@@ -7,11 +7,16 @@ function App() {
   // useState()사용해보기.
   let [blogTitles, blogEdit] = useState(['가을 노래 추천','인절미 호떡 맛나게 굽는 법', '공부를 열심히 해야하는 3가지 이유']);
   let [like, likeCount] = useState(0);
+  let [modal, setModal] = useState(false);
 
   function sortBlogTitles(){
     const cloneblogTitles = [...blogTitles];
     cloneblogTitles.sort();
     blogEdit(cloneblogTitles);
+  }
+
+  function stateModal(){
+    setModal(!modal);
   }
 
   return (
@@ -35,8 +40,24 @@ function App() {
          <h2> { blogTitles[2] } </h2>
          <span>2020년 11월 5일</span>
       </div>
+  <button type="button" onClick= { stateModal }> 글쓰기 </button>
+      {
+        modal === true ?  <Modal></Modal> : null
+      }
+
+       
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <span>날짜</span>
+      <p>싱세내용</p>
+    </div>
+  )
 }
 
 export default App;
