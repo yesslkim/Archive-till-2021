4f2b4beabf414a6a2a1845a0b35417b92670d6f6
yesslkim <yskim940914@gmail.com>
2020-11-12 12:18:44 +0900

    Create data.js to sepereate data

---
+++ assets/JS/data.js
@@ -0,0 +1,34 @@
+const db = firebase.firestore();
+
+//fetching art data 
+const renderArts = async () => {
+  let uri = 'https://my-json-server.typicode.com/yesslkim/monet-garden-api/arts';
+  const res = await fetch(uri);
+  const arts = await res.json();
+  
+  arts.forEach(art => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    if(art.id <= 10){
+      const ul = document.querySelector('.gallery_top');     
+      img.src = art.image;
+      img.alt = art.title;
+      li.append(img);
+      ul.append(li);
+    }else if(art.id <= 20){
+      const ul = document.querySelector('.gallery_middle');
+      img.src = art.image;
+      img.alt = art.title;
+      li.append(img);
+      ul.append(li);
+    }else {
+      const ul = document.querySelector('.gallery_bottom');
+      img.src = art.image;
+      img.alt = art.title;
+      li.append(img);
+      ul.append(li);
+    }
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => renderArts())
