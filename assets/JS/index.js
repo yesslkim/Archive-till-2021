39d5ee430c01a8096d7d84baf1bc79aefe4a8637
yesslkim <yskim940914@gmail.com>
2020-11-15 01:18:28 +0900

    Refactor to reduce global variable

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,39 +1,45 @@
-//scrolltop nav event 
-
-window.addEventListener('scroll',()=>{
-  const scrollTop = document.querySelector('.scrolltop');
-  if(window.pageYOffset>'100'){
-    scrollTop.classList.add('active')
-  }else {
-    scrollTop.classList.remove('active')
-  }
-})
-
-//navigation menu event 
-const gnbButton = document.querySelector('.gnb_btn');
-
-gnbButton.addEventListener('click', () => {
-	const gnbList = document.querySelector('.gnb_list');
-	gnbList.classList.toggle('active')
-})
-
-
+// global variable 
 const loginMenu = document.querySelector('.login_link');
 const logoutMenu = document.querySelector('.logout_link');
-
-//in login modal
 const loginModal = document.querySelector('.login_wrapper');
-const loginButton = document.querySelector('.login_btn');
-const closeButton = document.querySelector('.close_btn');
 
-loginMenu.addEventListener('click', ()=>{
-  loginModal.classList.add('active');
-})
+//scrolltop nav event 
+const scrollTopEvent = () => {
+  window.addEventListener('scroll',()=>{
+    const scrollTop = document.querySelector('.scrolltop');
+    if(window.pageYOffset>'100'){
+      scrollTop.classList.add('active')
+    }else {
+      scrollTop.classList.remove('active')
+    }
+  })
+}
 
-closeButton.addEventListener('click', ()=>{
-  loginModal.classList.remove('active');
-})
+//global navigation menu event 
+const gnbEvent = () => {
+  const gnbButton = document.querySelector('.gnb_btn');
 
+  gnbButton.addEventListener('click', () => {
+    const gnbList = document.querySelector('.gnb_list');
+    gnbList.classList.toggle('active')
+  })
+}
+
+//login modal Event
+const loginModalEvent = () => {
+  const loginModal = document.querySelector('.login_wrapper');
+  const closeButton = document.querySelector('.close_btn');
+
+  loginMenu.addEventListener('click', ()=>{
+    loginModal.classList.add('active');
+  })
+  
+  closeButton.addEventListener('click', ()=>{
+    loginModal.classList.remove('active');
+  })
+}
+
+//login/Logout Status UI
 const statusUI = (user)=> {
   if(user){
     loginMenu.parentElement.classList.add('hidden');
@@ -43,3 +49,9 @@ const statusUI = (user)=> {
     logoutMenu.parentElement.classList.add('hidden');
   }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  scrollTopEvent()
+  gnbEvent()
+  loginModalEvent();
+})
