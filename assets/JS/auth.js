2b5609e6c478561903191a264f74ac70aafbd34c
yesslkim <yskim940914@gmail.com>
2020-11-15 01:18:58 +0900

    Refactor to reduce global variable

--- assets/JS/auth.js
+++ assets/JS/auth.js
@@ -1,55 +1,53 @@
-// web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDnGOqRUDdgsQaChFx64sXFuUIDgUoxqxk",
-  authDomain: "monet-garden.firebaseapp.com",
-  databaseURL: "https://monet-garden.firebaseio.com",
-  projectId: "monet-garden",
-  storageBucket: "monet-garden.appspot.com",
-  messagingSenderId: "692803663657",
-  appId: "1:692803663657:web:5abd1938cf7ec9b5252938"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.auth();
-firebase.firestore();
-
+// global variable 
 const auth = firebase.auth();
 
-// auth status 
-auth.onAuthStateChanged(user=>{
-  if(user){
-    statusUI(user);
-  }else {
-    statusUI();
-  }
-})
-
-//login
-const loginForm = document.querySelector('.login_form');
-loginForm.addEventListener('submit',(e)=>{
-  e.preventDefault();
-  const email = loginForm.login_id.value;
-  const password = loginForm.login_pwd.value;
-  const loginError = document.querySelector('.login_error');
-
-  auth.signInWithEmailAndPassword(email,password).then(cred=>{
-    loginModal.classList.remove('active');
-
-    // reset login form 
-    loginForm.reset();
-    loginError.classList.remove('active');
-
-   }).catch(error=>{
-    const errorCode = error.code;
-    if(errorCode){
-      loginError.classList.add('active')
+const authStateEvent = () => {
+  auth.onAuthStateChanged(user=>{
+    if(user){
+      statusUI(user);
+    }else {
+      statusUI();
     }
-    console.log(error.message)
-  })  
+  })
+}
+
+const loginStateEvent = () => {
+  const loginForm = document.querySelector('.login_form');
+  loginForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const email = loginForm.login_id.value;
+    const password = loginForm.login_pwd.value;
+    const loginError = document.querySelector('.login_error');
+  
+    auth.signInWithEmailAndPassword(email,password).then(cred=>{
+      loginModal.classList.remove('active');
+  
+      loginForm.reset();
+      loginError.classList.remove('active');
+  
+     }).catch(error=>{
+      const errorCode = error.code;
+      if(errorCode){
+        loginError.classList.add('active')
+      }
+      console.log(error.message)
+    })  
+  })
+}
+
+const logoutStateEvent = () =>{
+  logoutMenu.addEventListener('click',(e)=>{
+    e.preventDefault();
+    auth.signOut();
+  })
+} 
+
+window.addEventListener('DOMContentLoaded', () => {
+  authStateEvent();
+  loginStateEvent();
+  logoutStateEvent();
 })
 
-//logout
-logoutMenu.addEventListener('click',(e)=>{
-  e.preventDefault();
-  auth.signOut();
-})
+
+
+
