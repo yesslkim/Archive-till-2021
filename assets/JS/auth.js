d12488d7bc159dc791d426e9438c88801ec32126
yesslkim <yskim940914@gmail.com>
2020-11-15 21:11:43 +0900

    Add for set data in orders

--- assets/JS/auth.js
+++ assets/JS/auth.js
@@ -6,7 +6,7 @@ const authStateEvent = () => {
   auth.onAuthStateChanged(user=>{
     if(user){
       statusUI(user);
-      db.collection('posts').onSnapshot(snapshot=>{
+      db.collection('posts').orderBy('createdAt').onSnapshot(snapshot=>{
         socialWallUI(snapshot.docs);
       })
     }else {
