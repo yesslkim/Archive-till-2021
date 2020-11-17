44072cae9cc1cc8cc57b903b7f497834e9eb2bd5
yesslkim <yskim940914@gmail.com>
2020-11-18 01:04:19 +0900

    Add layout for song component

---
+++ react-to-music/src/components/song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+  return(
+    <div className="song-container">
+      <span>picture</span>
+      <h3>song name</h3>
+      <span>artist</span>
+    </div>
+  )
+}
+
+export default Song;
