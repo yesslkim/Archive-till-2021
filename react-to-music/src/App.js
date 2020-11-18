e8b954bb77743c82b6774414fbecc6eb0cd277bf
yesslkim <yskim940914@gmail.com>
2020-11-19 00:51:13 +0900

    Add hooks and get data

--- react-to-music/src/App.js
+++ react-to-music/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 // style 
 import './styles/app.scss';
 // components 
 import Player from './components/player';
 import Song from './components/song';
+//util
+import music from './data';
 
 function App() {
+  const [songs,setSongs] = useState(music());
+  const [curretSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={curretSong}/>
       <Player />
     </div>
   );
