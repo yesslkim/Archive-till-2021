b5e3c50cbec34af03ecc91c2cd84c250516c92c4
yesslkim <yskim940914@gmail.com>
2020-11-19 01:06:20 +0900

    Add reference to the play button

--- react-to-music/src/components/player.js
+++ react-to-music/src/components/player.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({currentSong}) => {
+  const audioRef = useRef(null);
+  //event
+  const playSongHandler = () => {
+    console.log(audioRef.current)
+  }
   return(
     <div className="player-container">
       <div className="time-control">
@@ -12,9 +17,10 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="prev" size="2x" icon={faAngleLeft}/>
-        <FontAwesomeIcon className="play" size="2x"  icon={faPlay}/>
+        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x"  icon={faPlay}/>
         <FontAwesomeIcon className="next" size="2x"  icon={faAngleRight}/>
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   )
 }
