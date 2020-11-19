a3ba586b2a0420cd21e10b743ead07a6024ef06a
yesslkim <yskim940914@gmail.com>
2020-11-19 16:26:00 +0900

    Add header component

---
+++ react-to-music/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>{
+  return (
+    <header className="header">
+      <h1>React to Music</h1>
+      <button type="button">
+        Library
+      </button>
+    </header>
+  )
+}
+
+export default Header;
