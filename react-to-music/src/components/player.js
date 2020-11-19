4d261c09b536021883edd76c9a11981d861ad6ae
yesslkim <yskim940914@gmail.com>
2020-11-19 11:53:51 +0900

    Delete state and variable to lift up

--- react-to-music/src/components/player.js
+++ react-to-music/src/components/player.js
@@ -1,9 +1,8 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPause, faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 
-const Player = ({currentSong, isPlaying, setIsPlaying}) => {
-  const audioRef = useRef(null);
+const Player = ({currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setSongInfo}) => {
   //event
   const playSongHandler = () => {
     if(isPlaying){
@@ -15,12 +14,6 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
     }
   }
 
-  const timeUpdateHandler = (e) => {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    setSongInfo({...songInfo, currentTime: current, duration})
-  }
-
   const getTime = (time) => {
     return Math.floor(time/60)+ ':' + ("0" + Math.floor(time % 60)).slice(-2);
   }
@@ -30,11 +23,12 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
     setSongInfo({...songInfo, currentTime: e.target.value})
   }
 
-  //state
-  const [songInfo, setSongInfo] = useState({
-    currentTime: 0,
-    duration: 0
-  })
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({...songInfo, currentTime: current, duration})
+  }
+
   return(
     <div className="player-container">
       <div className="time-control">
