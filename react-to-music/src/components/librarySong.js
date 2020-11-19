2bb76a14ce43192ec35df151b365b1a772fd90d4
yesslkim <yskim940914@gmail.com>
2020-11-19 13:55:32 +0900

    Add event to get bg color when song is active

--- react-to-music/src/components/librarySong.js
+++ react-to-music/src/components/librarySong.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-const LibrarySong = ({song,setCurrentSong,audioRef,isPlaying}) => {
+const LibrarySong = ({song, songs, setCurrentSong,audioRef,isPlaying, setSongs}) => {
   const songSelectHandler = () =>{
     setCurrentSong(song);
+    //add active state
+    const activeStateOfSongs = songs.map(eachSong=> {
+      if(eachSong.id === song.id){
+        return {
+          ...eachSong,
+          active: true,
+        }
+      }else {
+        return {
+          ...eachSong,
+          active: false
+        }
+      }
+    })
+    setSongs(activeStateOfSongs);
+
+    // to play song at right time
     if(isPlaying){
       const playPromise = audioRef.current.play();
       if(playPromise !== undefined){
         playPromise.then(() => audioRef.current.play())
       }
-    }else{
-      
     }
   }
   return(
-    <div onClick={songSelectHandler} className="library-song">
+    <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ''}`}>
       <img src={song.cover} alt={song.name}/>
       <div className="song-desc">
         <h3>{song.name}</h3>
