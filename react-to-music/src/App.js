4cb06d97252c1766b830a244499ee39000e52676
yesslkim <yskim940914@gmail.com>
2020-11-19 11:52:12 +0900

    Add useRef to reuse in another component

--- react-to-music/src/App.js
+++ react-to-music/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 // style 
 import './styles/app.scss';
 // components 
@@ -9,14 +9,20 @@ import Library from './components/library'
 import music from './data';
 
 function App() {
+  const audioRef = useRef(null);
+
   const [songs,setSongs] = useState(music());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+      currentTime: 0,
+      duration: 0
+  })
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
-      <Library songs={songs}/>
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} audioRef={audioRef} songInfo={songInfo} setSongInfo={setSongInfo} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} audioRef={audioRef} isPlaying={isPlaying} />
     </div>
   );
 }
