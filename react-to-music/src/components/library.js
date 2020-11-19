57d44187619f630118d0740670d877a0b35c3e41
yesslkim <yskim940914@gmail.com>
2020-11-19 11:52:23 +0900

    Add props

--- react-to-music/src/components/library.js
+++ react-to-music/src/components/library.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import LibrarySong from './librarySong';
 
- const Library = ({songs}) =>{
+ const Library = ({songs, setCurrentSong, audioRef, isPlaying}) =>{
    return(
      <div className="music-library">
        <h2>Library</h2>
        <div className="library">
          {
-           songs.map(song => <LibrarySong song={song}/>)
+           songs.map(song => <LibrarySong key={song.id} song={song} setCurrentSong={setCurrentSong} audioRef={audioRef} isPlaying={isPlaying}/>)
          }
-         
        </div>
      </div>
    )
