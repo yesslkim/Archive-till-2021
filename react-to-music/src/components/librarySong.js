a35b93aac0c12cf5c0c1044c4abae4c9c3a83883
yesslkim <yskim940914@gmail.com>
2020-11-19 11:52:45 +0900

    Add promise to play song in right time

--- react-to-music/src/components/librarySong.js
+++ react-to-music/src/components/librarySong.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({song,setCurrentSong,audioRef,isPlaying}) => {
+  const songSelectHandler = () =>{
+    setCurrentSong(song);
+    if(isPlaying){
+      const playPromise = audioRef.current.play();
+      if(playPromise !== undefined){
+        playPromise.then(() => audioRef.current.play())
+      }
+    }else{
+      
+    }
+  }
   return(
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name}/>
       <div className="song-desc">
         <h3>{song.name}</h3>
