600266bd31ee81bb3578bbfd6c0790568b4d5ff2
yesslkim <yskim940914@gmail.com>
2020-11-19 18:31:38 +0900

    Create function for play audio

---
+++ react-to-music/src/util.js
@@ -0,0 +1,8 @@
+export const playAudio = (isPlaying, audioRef)=>{
+  if(isPlaying){
+    const playPromise = audioRef.current.play();
+    if(playPromise !== undefined){
+      playPromise.then(() => audioRef.current.play())
+    }
+  }
+}
