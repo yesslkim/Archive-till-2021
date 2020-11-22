dddd0031a428ff92a8dd21ec0e3eb50653e0cfe2
yesslkim <yskim940914@gmail.com>
2020-11-22 19:56:36 +0900

    Add 로그인 팝업창 오픈시 스크롤방지기능

--- assets/JS/header.js
+++ assets/JS/header.js
@@ -2,6 +2,7 @@
 const loginMenu = document.querySelector('.login_link');
 const logoutMenu = document.querySelector('.logout_link');
 const loginModal = document.querySelector('.login_wrapper');
+const loginHelp = document.querySelector('.login_help');
 
 //scrolltop nav event 
 const scrollTopEvent = () => {
@@ -28,19 +29,20 @@ const gnbEvent = () => {
 //login modal Event
 const loginModalEvent = () => {
   const loginModal = document.querySelector('.login_wrapper');
-  const loginHelp = document.querySelector('.login_help');
   const loginArea = document.querySelector('.login_area');
   const closeButton = document.querySelector('.login_area > .close_btn');
 
   loginMenu.addEventListener('click', ()=>{
     loginModal.classList.add('active');
     loginHelp.classList.add('active');
+    disableScroll();
   })
 
   loginArea.addEventListener('click', (e)=>{
     if(e.target === closeButton){
       loginModal.classList.remove('active');
       loginHelp.classList.remove('active');
+      enableScroll();
     }
   })
 
@@ -50,6 +52,15 @@ const loginModalEvent = () => {
     }
   })
 }
+
+function disableScroll() {
+  document.querySelector('body').classList.add('lockscroll')
+  document.querySelector('html').offsetTop = window.scrollY;
+  }
+  
+function enableScroll() {
+  document.querySelector('body').classList.remove('lockscroll')
+  }
 
 //login/Logout Status UI
 const statusUI = (user)=> {
