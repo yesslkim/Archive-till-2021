44902f236911f485b49b74bdc59a5f226eba9b1c
yesslkim <yskim940914@gmail.com>
2020-11-22 19:47:26 +0900

    Fix 로그인전용 글쓰기 비로그인시 활성화 차단

--- assets/JS/socialwall.js
+++ assets/JS/socialwall.js
@@ -23,16 +23,17 @@ const socialWallUI = (dataLists) =>{
   })
 }
 
+
 //게시판 데이터 보내기 
 const createNewPost = () => {
   const postForm = document.querySelector('.post_form');
-
   postForm.addEventListener('submit',(e)=>{
-    const user = auth.currentUser;
+    e.preventDefault();
+
+    // const user = auth.currentUser;
     const date = new Date().toLocaleDateString();
     const timestamp = firebase.firestore.FieldValue.serverTimestamp;
-   
-    e.preventDefault();
+    const user = auth.currentUser;
 
     db.collection('posts').add({
       title: postForm['post_title'].value,
@@ -46,9 +47,6 @@ const createNewPost = () => {
       postForm.reset();
     }).catch(error=>{
       console.log(error.message)
-      if(error.message ==='Missing or insufficient permissions.'){
-        alert('로그인 후 글쓰기가 가능합니다.')
-      }
     }) 
   })
 }
@@ -56,10 +54,15 @@ const createNewPost = () => {
 // 게시글 글쓰기 열기
 const openPostForm = () => {
   const socialWall = document.querySelector('.socialwall');
-
   socialWall.addEventListener('click', (e)=>{
-    if(e.target.className === 'write_btn'){
-      postForm.classList.add('active');
+    if(e.target.className !== 'write_btn')return;
+    console.log(isOnline)
+    if(isOnline){
+        postForm.classList.add('active');
+        createNewPost();
+        closeUI(postForm);
+    }else{
+      alert('로그인 후 글쓰기가 가능합니다.')
     }
   })
 }
@@ -80,43 +83,65 @@ const openPostModal = () => {
     const targetPostUserID = postTitle.nextElementSibling.textContent;
 
     if(postTitle.className ==='title'){
-
-      db.collection('posts').orderBy('createdAt').get().then(snapshot =>{
+      if(!isOnline){
+        db.collection('posts').orderBy('createdAt').get().then(snapshot=>{
+          const posts = snapshot.docs;
+          posts.filter((post,index)=>{
+            const postIndex = (index + 1).toString();
+            return postIndex === targetPostIndex;
+          }).map((post,index)=>{
+            const targetPost = post.data()
+            const postIndex = (index + 1).toString();
+            postModal.classList.add('active')
+            increaseView(post);
+            return(
+              postModal.innerHTML = `
+              <button type="button" class="close_btn">닫기</button>
+              <h3>${targetPost.title}</h3>
+              <p>
+              ${targetPost.content}
+              </p>
+              <span>-${postIndex}-</span>
+              `
+            )
+          })
+          closeUI(postModal);
+        }).catch(error=> console.log(error.message))
+      }else {
+        db.collection('posts').orderBy('createdAt').get().then(snapshot=>{
         const posts = snapshot.docs;
-  
         posts.forEach((post,index)=>{
           const postIndex = (index + 1).toString();
           const targetPost = post.data()
-
-          if(targetPostIndex === postIndex && targetPostUserID === auth.currentUser.email){
-            postModal.innerHTML = `
-            <button type="button" class="close_btn">닫기</button>
-            <button type="button" class="delete_btn">삭제하기</button>
-            <h3>${targetPost.title}</h3>
-            <p>
-            ${targetPost.content}
-            </p>
-            <span>-${postIndex}-</span>
-            `
-            postModal.classList.add('active');
-
-            increaseView(post);
-          }else if (targetPostIndex === postIndex){
-            postModal.innerHTML = `
-            <button type="button" class="close_btn">닫기</button>
-            <h3>${targetPost.title}</h3>
-            <p>
-            ${targetPost.content}
-            </p>
-            <span>-${postIndex}-</span>
-            `
-            postModal.classList.add('active')
-            increaseView(post);
-          }
+            if(targetPostIndex === postIndex && targetPostUserID === auth.currentUser.email){
+              postModal.innerHTML = `
+              <button type="button" class="close_btn">닫기</button>
+              <button type="button" class="delete_btn">삭제하기</button>
+              <h3>${targetPost.title}</h3>
+              <p>
+              ${targetPost.content}
+              </p>
+              <span>-${postIndex}-</span>
+              `
+              postModal.classList.add('active');
+  
+              increaseView(post);
+            }else if (targetPostIndex === postIndex){
+              postModal.innerHTML = `
+              <button type="button" class="close_btn">닫기</button>
+              <h3>${targetPost.title}</h3>
+              <p>
+              ${targetPost.content}
+              </p>
+              <span>-${postIndex}-</span>
+              `
+              postModal.classList.add('active')
+              increaseView(post);
+            }
         })
-      }).catch(error=>{
-        console.log(error.message)
-      })
+        closeUI(postModal);
+      }).catch(error=> console.log(error.message))
+      }
     }
   })
 }
@@ -132,15 +157,15 @@ const closeUI = (targetUI) => {
   }) 
 }
 
-const displayPostForm = () => {
-  openPostForm();
-  closeUI(postForm);
-}
+// const displayPostForm = (user) => {
+//   openPostForm(user);
+//   closeUI(postForm);
+// }
 
-const displayPostModal = () => {
-  openPostModal();
-  closeUI(postModal);
-}
+// const displayPostModal = (user) => {
+//   openPostModal(user);
+//   closeUI(postModal);
+// }
 
 const deletePost = () => {
   postModal.addEventListener('click',(e)=>{
@@ -170,8 +195,6 @@ const deletePost = () => {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  displayPostForm();
-  displayPostModal();
-  createNewPost();
+  openPostModal();
   deletePost();
 })
