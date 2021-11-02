c38be3c13dc920c5c46c268bee51e535cebe108d
Ye Seul Kim <yskim940914@gmail.com>
2021-11-02 20:32:58 +0900

    Add fullpage javascript for pagination

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,8 +15,17 @@ $(function () {
       // SECTION별 로고, NAV색 변경
       if (destination == 3 || destination == 4) {
         $(".logo a , .main-nav > li, .lang").addClass("active");
+        $("#fp-nav ul li a span").addClass("dark");
       } else {
         $(".logo a, .main-nav > li, .lang").removeClass("active");
+        $("#fp-nav ul li a span").removeClass("dark");
+      }
+
+      //FOOTER PAGINATION STYLE
+      if (destination == 6) {
+        $("#fp-nav ul li").eq(4).children("a").addClass("show");
+      } else {
+        $("#fp-nav ul li").eq(4).children("a").removeClass("show");
       }
     },
   });
