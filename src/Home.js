7ba21b6c8b473f0ff3c5d8ab2e98eede0d06ebb1
Ye Seul Kim <yskim940914@gmail.com>
2021-12-28 23:51:25 +0900

    기본적인 handling event에 대해 학습

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = (e) => { 
+    console.log('testing 😎')
+    e.target.style.backgroundColor = 'yellow';
+  }
+
+  const handleClickAgain = (name) => { 
+    console.log(`hello ${name}!`);
+  }
+
   return ( 
     <div className="home">
       <h2>Hompage</h2>
+      <button onClick={ handleClick } type="button">Click Me!</button>
+      <button onClick={ (e) => handleClickAgain('kim',e) } type="button">Click Me! Again!</button>
     </div>
    );
 }
