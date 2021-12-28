3dd89cb40c0a5a94ddd6863da2ece6238bc54527
Ye Seul Kim <yskim940914@gmail.com>
2021-12-28 18:49:49 +0900

    새로운 컴포넌트 생성하는 법 공부

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return ( 
+    <div className="home">
+      <h2>Hompage</h2>
+    </div>
+   );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Kim's Blog</h1>
+      <div className="links">
+        <a href="/">HOME</a>
+        <a href="/create">NEW BLOG</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
