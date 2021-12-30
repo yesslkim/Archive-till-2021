b1bbf22a4a323164ac03d5a839eeb2f662bbdcf1
Ye Seul Kim <yskim940914@gmail.com>
2021-12-31 02:15:12 +0900

    Lists and Keys에 대해 공부

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,22 @@
 import { useState } from 'react';
 
 const Home = () => {
-  // let writer = 'stranger';
-  const [writer, setWriter] = useState('stranger');
-  const [age, setAge] = useState(100);
 
-  const handleClick = () => { 
-    setWriter('kim');
-    setAge(200);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'First blog! 😎', body: 'lorem ipsum...', author: 'mario', id: 1},
+    { title: 'Hello There! 😉', body: 'lorem ipsum...', author: 'kim', id: 2},
+    { title: 'Web Tips 🏃‍♀️🏃‍♀️', body: 'lorem ipsum...', author: 'lee', id: 3}
+  ]);
 
-  const handleClickAgain = (name) => { 
-    console.log(`hello ${name}!`);
-  }
 
   return ( 
     <div className="home">
-      <h2>Hompage</h2>
-      <p>{writer} is {age} years old 😁</p>
-      <button onClick={ handleClick } type="button">Click Me!</button>
-      <button onClick={ (e) => handleClickAgain('kim',e) } type="button">Click Me! Again!</button>
+      {blogs.map(blog => (
+        <a className="blog-preview" href="/" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p> Written By { blog.author} </p>
+        </a>
+      )) }
     </div>
    );
 }
