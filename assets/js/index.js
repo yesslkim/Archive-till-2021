87d0bb2cb4e19d1d65abfe7c83b5c7f72d1ab392
Ye Seul Kim <yskim940914@gmail.com>
2022-01-03 15:47:26 +0900

    Change portfolio style and layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,246 +1,161 @@
+// VARIABLE
 let tl = gsap.timeline();
 const headerTop = document.querySelector(".header-top");
 
-const refreshOnResize = () => {
-  window.addEventListener("resize", () => {
-    window.location.reload();
-  });
-};
+// HEADER - GNB
+headerTop.addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
 
-const matchMedia = {
-  desktop: {
-    y:
-      document.querySelector(".about-image").clientHeight -
-      document.querySelector(".bowl").clientHeight +
-      80,
-    otherY: document.querySelector(".about-image").clientHeight + 70,
-  },
-  tablet: {
-    y:
-      document.querySelector(".about-image").clientHeight -
-      document.querySelector(".bowl").clientHeight +
-      40,
-    otherY: document.querySelector(".about-image").clientHeight + 40,
-  },
-};
+  if (e.target.classList.contains("gnb-btn")) {
+    document.body.classList.add("disable-scroll");
 
-function onComplete() {
-  let index = 0;
-
-  document.querySelectorAll(".about-image > div").forEach((apple, i) => {
-    if (apple.classList.contains("target")) {
-      index = i + 1;
-      document.querySelector(".target").classList.remove("target");
-      return index;
-    }
-  });
-
-  document.querySelectorAll(".about-list li").forEach((list) => {
-    list.classList.remove("show");
-    document.querySelectorAll(".about-list li")[index].classList.add("show");
-  });
-}
-
-const mainTimeline = (y, othery) => {
-  const sectionTimeline = gsap.timeline();
-  sectionTimeline
-    .to(".html-apple", {
-      y: y,
-      rotate: 360,
-      onStart: () => {
-        document.querySelector(".html-apple").classList.add("target");
-      },
-      onComplete: onComplete,
-      onReverseComplete: () => {
-        document.querySelectorAll(".about-list li").forEach((list) => {
-          list.classList.remove("show");
-        });
-        document.querySelector(".about-default").classList.add("show");
-      },
-    })
-    .to(".css-apple", {
-      y: y,
-      rotate: 360,
-      onStart: () => {
-        document.querySelector(".css-apple").classList.add("target");
-      },
-      onComplete: onComplete,
-      onReverseComplete: () => {
-        document.querySelector(".html-apple").classList.add("target");
-        onComplete();
-      },
-    })
-    .to(".js-apple", {
-      y: y,
-      rotate: 360,
-      onStart: () => {
-        document.querySelector(".js-apple").classList.add("target");
-      },
-      onComplete: onComplete,
-      onReverseComplete: () => {
-        document.querySelector(".css-apple").classList.add("target");
-        onComplete();
-      },
-    })
-    .to(".others-apple", {
-      y: othery,
-      rotate: 360,
-      onStart: () => {
-        document.querySelector(".others-apple").classList.add("target");
-      },
-      onComplete: onComplete,
-      onReverseComplete: () => {
-        document.querySelector(".js-apple").classList.add("target");
-        onComplete();
-      },
-    })
-    .from(".projects", {
-      delay: 0.5,
-      opacity: 0,
-      yPercent: 100,
-    })
-    .to(".about-content", {
-      opacity: 0,
-    });
-
-  ScrollTrigger.create({
-    animation: sectionTimeline,
-    trigger: ".main",
-    start: "top top",
-    end: "+=4000",
-    pin: true,
-    scrub: true,
-    anticipatePin: 1,
-    toggleActions: "restart none reverse none",
-  });
-};
-
-function scrollToProject() {
-  headerTop.addEventListener("click", (e) => {
-    if (e.target.tagName !== "A") return;
-    if (e.target.classList.contains("project-link")) {
-      gsap.to(window, {
-        duration: 1,
-        scrollTo: { y: "#projects", offsetY: -1400 },
-      });
-    }
-  });
-}
-
-ScrollTrigger.matchMedia({
-  // DESKTOP
-  "(min-width: 1024px)": function () {
-    ScrollTrigger.saveStyles(".main, .about, .projects");
-    mainTimeline(matchMedia.desktop.y, matchMedia.desktop.otherY);
-    scrollToProject();
-    refreshOnResize();
-    gsap.from(".hero", {
-      duration: 1,
-      opacity: 0,
-      y: 310,
-    });
-  },
-
-  // TABLET
-  "(min-width: 768px) and (max-width: 1023px)": function () {
-    ScrollTrigger.saveStyles(".main, .about, .projects");
-    mainTimeline(matchMedia.tablet.y, matchMedia.tablet.otherY);
-    scrollToProject();
-    refreshOnResize();
-  },
-
-  // MOBILE
-  "(min-width: 320px) and (max-width: 767px)": function () {
-    // gsap.to(".smile-wrapper", {
-    //   scrollTrigger: {
-    //     trigger: ".header",
-    //     scrub: true,
-    //     start: "top top",
-    //     end: "+=100% 0px",
-    //     pin: true,
-    //   },
-    //   rotate: 360,
-    //   y: 210,
-    //   scale: 1.25 / 0.75,
-    // });
-  },
-
-  // DEVICE COMMON
-  all: function () {
-    //HEADER
-    ScrollTrigger.saveStyles(".gnb, .gnb li");
-
-    headerTop.addEventListener("click", (e) => {
-      if (e.target.tagName !== "BUTTON") return;
-
-      if (e.target.classList.contains("gnb-btn")) {
-        tl.fromTo(
-          ".gnb",
-          {
-            width: 0,
-            opacity: 0,
-          },
-          {
-            duration: 0.5,
-            opacity: 1,
-            width: "100%",
-          }
-        ).fromTo(
-          ".gnb li",
-          {
-            display: "block",
-            opacity: 0,
-            y: "50%",
-          },
-          {
-            duration: 1,
-            opacity: 1,
-            y: "0",
-            stagger: 0.25,
-            ease: "power4.out",
-          }
-        );
-      }
-
-      if (e.target.classList.contains("close-btn")) {
-        tl.to(".gnb li", {
-          duration: 0.1,
-          display: "block",
-          opacity: 0,
-          y: "50%",
-          stagger: {
-            each: 0.25,
-            from: "end",
-          },
-        }).to(".gnb", {
-          width: 0,
-          opacity: 0,
-        });
-      }
-    });
-
-    headerTop.addEventListener("click", (e) => {
-      if (e.target.tagName !== "A") return;
-
-      gsap.to(".gnb", {
+    tl.fromTo(
+      ".gnb",
+      {
         width: 0,
         opacity: 0,
-      });
+      },
+      {
+        duration: 0.5,
+        opacity: 1,
+        width: "100%",
+      }
+    ).fromTo(
+      ".gnb li",
+      {
+        display: "block",
+        opacity: 0,
+        y: "50%",
+      },
+      {
+        duration: 1,
+        opacity: 1,
+        y: "0",
+        stagger: 0.25,
+        ease: "power4.out",
+      }
+    );
+  }
+
+  if (e.target.classList.contains("close-btn")) {
+    tl.to(".gnb li", {
+      duration: 0.1,
+      display: "block",
+      opacity: 0,
+      y: "50%",
+      stagger: {
+        each: 0.25,
+        from: "end",
+      },
+    }).to(".gnb", {
+      width: 0,
+      opacity: 0,
     });
 
-    //MAIN - ABOUT-SECTION
+    document.body.classList.remove("disable-scroll");
+  }
+});
 
-    document.querySelector(".about-list").addEventListener("click", (e) => {
-      if (e.target.tagName !== "BUTTON") return;
+headerTop.addEventListener("click", (e) => {
+  if (e.target.tagName !== "A") return;
 
-      document.querySelectorAll(".skill-btn").forEach((btn) => {
-        if (btn.parentElement.classList.contains("active")) {
-          btn.parentElement.classList.remove("active");
-        } else {
-          btn.parentElement.classList.remove("active");
-          btn === e.target && btn.parentElement.classList.add("active");
-        }
-      });
+  gsap.to(".gnb", {
+    width: 0,
+    opacity: 0,
+  });
+
+  document.body.classList.remove("disable-scroll");
+});
+
+// MAIN - ABOUT-SECTION
+document.querySelector(".about-skills").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  document.querySelectorAll(".skill-btn").forEach((btn) => {
+    if (btn.parentElement.classList.contains("active")) {
+      btn.parentElement.classList.remove("active");
+    } else {
+      btn.parentElement.classList.remove("active");
+      btn === e.target && btn.parentElement.classList.add("active");
+    }
+  });
+});
+
+// MAIN - PROJECT-SECTION
+document.querySelector(".project-contents").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  if (e.target.classList.contains("summary")) {
+    document.querySelector(".project-contents .inner").classList.remove("left");
+    document.querySelector(".project-contents .inner").classList.add("right");
+
+    document.querySelectorAll(".project").forEach((project) => {
+      project.classList.remove("active");
+
+      if (e.target.dataset.target === project.dataset.target) {
+        project.classList.add("active");
+      }
     });
-  },
+  }
+
+  if (e.target.classList.contains("back-btn")) {
+    document
+      .querySelector(".project-contents .inner")
+      .classList.remove("right");
+    document.querySelector(".project-contents .inner").classList.add("left");
+  }
+});
+
+// BG SETTINGS
+document.querySelector(".setting1").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  const colorName = e.target.getAttribute("class");
+
+  document.querySelectorAll(".setting2 button").forEach((opacityBtn) => {
+    opacityBtn.style.backgroundColor = "#" + colorName;
+  });
+
+  document.body.style.backgroundColor = "#" + colorName;
+});
+
+document.querySelector(".setting2").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  let colorName = document.body.style.backgroundColor;
+  let colorRGB = colorName.slice(4, -1);
+  const opacity = e.target.textContent;
+
+  if (colorName == "") {
+    colorName = "rgb(236, 243, 255)";
+  } else if (colorName.slice(0, 4) == "rgba") {
+    colorRGB = colorName.slice(5, -6);
+  }
+
+  document.body.style.backgroundColor = `rgba(${colorRGB},${opacity})`;
+});
+
+// SCREEN OVERFLOW ISSUE
+window.addEventListener("resize", (e) => {
+  //screen
+  if (500 < window.innerWidth < 768) {
+    if (window.innerHeight < 800) {
+      document.querySelector(".smile-wrapper").classList.add("active");
+      document.querySelector(".header-bottom p").classList.add("active");
+    } else {
+      document.querySelector(".smile-wrapper").classList.remove("active");
+      document.querySelector(".header-bottom p").classList.remove("active");
+    }
+  } else if (window.innerWidth > 768) {
+    if (window.innerHeight < 901) {
+      document.querySelector(".smile-wrapper").classList.add("active");
+      document.querySelector(".header-bottom p").classList.add("active");
+    } else {
+      document.querySelector(".smile-wrapper").classList.remove("active");
+      document.querySelector(".header-bottom p").classList.remove("active");
+    }
+  } else if (window.innerWidth < 500) {
+    document.querySelector(".smile-wrapper").classList.remove("active");
+    document.querySelector(".header-bottom p").classList.remove("active");
+  }
 });
