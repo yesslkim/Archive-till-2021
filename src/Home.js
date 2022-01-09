8092a550e694a8d9b3309f693fa3d38a8ce649b3
Ye Seul Kim <yskim940914@gmail.com>
2022-01-09 18:09:41 +0900

    custom hook 만드는 방법 학습

--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
-
-  // useEffect(() => {
-  //   console.log("🙄💜", name);
-  // }, [name]);
-
-  // useEffect(() => {
-  //   console.log("처음 렌더링");
-  //   return () => {
-  //     console.log("name 바뀌었당!");
-  //   };
-  // }, [name]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
