6d9152b3d01e45c87da9698fd827e0ca8e80edc4
Ye Seul Kim <yskim940914@gmail.com>
2022-01-12 18:39:59 +0900

    Form Control 하는 방법 학습

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isLoading,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
-    <div className="blog-detail">
-      <h2>Blog Detail - {id}</h2>
-    </div>
+    <article className="blog-detail">
+      {error && <p className="err-msg">{error}</p>}
+      {isLoading && <p className="loading-msg">Loading...</p>}
+      {blog && (
+        <>
+          <h2>{blog.title}</h2>
+          <strong>wriiten by {blog.author}</strong>
+          <p>{blog.body}</p>
+        </>
+      )}
+    </article>
   );
 };
 
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("kim");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form className="blog-form" onSubmit={handleSubmit}>
+        <label htmlFor="">Blog Title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="">Blog body: </label>
+        <textarea
+          name=""
+          id=""
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label htmlFor="">Blog author: </label>
+        <select
+          name=""
+          id=""
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="kim">kim</option>
+          <option value="lee">lee</option>
+          <option value="park">park</option>
+        </select>
+        <button type="submit">Add Blog</button>
+      </form>
     </div>
   );
 };
